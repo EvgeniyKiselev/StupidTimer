dba6801d777ee3e390feac098bc0d11b94d06ac8
mr.kiseleff@list.ru <2yl0butaf1e5>
2022-02-26 03:09:58 +0300

    таймер на лямбдах

--- src/main/java/timer/App.java
+++ src/main/java/timer/App.java
@@ -3,9 +3,6 @@ package timer;
 public class App {
     public static void main(String args[]){
         Timer timer = new Timer();
-        System.out.println("Тупой таймер - поехали:");
-        timer.timer();
-        System.out.println("Таймер покрасивше - поехали:");
         timer.timerWithTimer();
     }
 }
--- src/main/java/timer/Timer.java
+++ src/main/java/timer/Timer.java
@@ -1,24 +1,46 @@
 package timer;
 
-public class Timer {
-    public void timer() {
-        for (int i = 0; i < 31; i++) {
-            if (i < 10) {
-                System.out.println("00:0" + i);
-            } else System.out.println("00:" + i);
-            try {
-                if (i != 30) {
-                    Thread.sleep(1000);
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
+public class Timer  {
 
     public void timerWithTimer() {
-        Thread mThread = new MyThread();
+        Thread mThread = new Thread(()->{
+            int count = 30;
+            for (int i = 0; i <= count; i++) {
+                System.out.print(timeWithWords.get());
+                System.out.println(" --- до окончания отсчета осталось: "
+                        + reverseCount.apply(count - i));
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         mThread.start();
+    }
+
+    Function<Integer, String> reverseCount = (i) -> {
+        if(i < 10){
+            return "0" + String.valueOf(i);
+        }
+        else return String.valueOf(i);
+    };
+
+    Supplier<String> timeWithWords = this::get;
+
+    Function<String, String> addWord = (str) -> {
+        String[] words = (str.split(":"));
+        StringBuilder bld = new StringBuilder();
+        return  bld.append("Часы: ").append(words[0]).append(" минуты: ")
+                .append(words[1]).append(" секунды: ").append(words[2]).toString();
+    };
+
+    private String get() {
+        return addWord.apply(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
     }
 }
